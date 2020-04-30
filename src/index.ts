b0ae9e55f1ecb7876154d0f576a5611a11665854
Gabriel Petry <petry@pop-os.localdomain>
2020-04-29 21:30:26 -0300

    this is a bad commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import fastify from "fastify"
-import fastifyHelmet from "fastify-helmet"
-import cors from "fastify-cors"
+import fastify from 'fastify'
+import fastifyHelmet from 'fastify-helmet'
+import cors from 'fastify-cors'
 
 const server = fastify()
 
 server.register(fastifyHelmet)
-server.register(cors, { origin: "*" })
+server.register(cors, { origin: '*' })
 
-server.listen(3000, '0.0.0.0')
-  .then(url => {
+import bookRouter from './routes/bookRouter'
+bookRouter.map((route) => server.route(route))
+
+server
+  .listen(3000, '0.0.0.0')
+  .then((url) => {
     console.log(`listening on ${url}`)
   })
-  .catch(err => {
+  .catch((err) => {
     if (err) throw err
   })
