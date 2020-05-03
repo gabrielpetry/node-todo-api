06570d6788b82e3dd19ea08e942030d535faf1f3
Gabriel Petry <contato@gabrilpetry.com.br>
2020-05-03 11:35:23 -0300

    database removed from index

---
+++ src/database.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+class Database {
+  private db_host = 'mongo'
+  private db_database = 'todo'
+  private db_user = 'root'
+  private db_password = 'example'
+
+  constructor() {}
+  private SetupDb(): any {
+    mongoose.connect(`mongodb://${this.db_host}:27017/${this.db_database}?authSource=admin`, {
+      user: this.db_user,
+      pass: this.db_password,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log(this.db_user)
+    console.log(this.db_password)
+    const db = mongoose.connection
+    db.on('error', console.error.bind(console, 'MongoDB Connection error'))
+    return db
+  }
+}
+
+export default Database
