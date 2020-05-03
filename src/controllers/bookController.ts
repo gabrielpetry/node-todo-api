a9359bb584ea8ac5af85a0f6176d50e88e23ff96
Gabriel Petry <contato@gabrilpetry.com.br>
2020-05-03 15:36:56 -0300

    return books on the get

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -5,14 +5,18 @@ import book from '../models/book'
 
 export default {
   async getBooks(req, reply) {
-    return { ok: true }
+    const books = await book.find()
+
+    return { success: true, books }
   },
 
   async store(req, reply) {
-    const books = await book.create({}, (err, book) => {
+    // console.log(req.body)
+    const { name, description, active = true } = req.body
+
+    const books = await book.create({ name, description, active }, (err, book) => {
       if (err) return reply.code(400).send(err)
       return reply.code(201).send()
     })
-    // return books
   },
 }
