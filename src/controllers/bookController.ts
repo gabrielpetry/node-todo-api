45fc31544bbab739bf93d2844eef090ddae3b390
Gabriel Petry <contato@gabrilpetry.com.br>
2020-05-04 12:27:46 -0300

    fi delete method

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -6,17 +6,21 @@ import book from '../models/book'
 export default {
   async getBooks(req, reply) {
     const books = await book.find()
-
     return { success: true, books }
   },
 
   async store(req, reply) {
-    // console.log(req.body)
     const { name, description, active = true } = req.body
 
     const books = await book.create({ name, description, active }, (err, book) => {
       if (err) return reply.code(400).send(err)
       return reply.code(201).send()
     })
+  },
+
+  async delete(req, reply) {
+    const { _id } = req.params
+    await book.deleteOne({ _id })
+    return reply.code(204).send()
   },
 }
