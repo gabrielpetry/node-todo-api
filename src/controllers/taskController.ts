d0be181a69b3a29c898502925aab49120dfa68ad
Gabriel Petry <contato@gabrilpetry.com.br>
2020-05-07 10:41:26 -0300

    fix name definition

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -3,17 +3,17 @@ import task from '../models/task'
 
 export default {
   async getTasks(req, reply) {
-    const todos = await task.find().populate('book')
-    return reply.send({ todos })
+    const tasks = await task.find({ book: req.params._id }).populate('book')
+    return reply.send({ tasks })
   },
 
   async store(req, reply) {
     const { _id: book } = req.params
     const { description } = req.body
 
-    await task.create({ description, book }, (err, todo) => {
+    await task.create({ description, book }, (err, task) => {
       if (err) return reply.code(400).send({ err })
-      reply.code(201).send({ todo })
+      reply.code(201).send({ task })
     })
   },
 
